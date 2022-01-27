d4e22e4fed7b99e53762cd164825d6c43e6a55b1
mfvargas <mfvargas@gmail.com>
2022-01-26 22:57:12 -0600

    Crear modelos/playapotrero-201106-schosinsky.ipynb

---
+++ _build/jupyter_execute/modelos/playapotrero-201106-schosinsky.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Balance hídrico de suelos
+
+# **Zona de estudio**: Playa Potrero, Santa Cruz - Guanacaste (Zona Bosques 4)  
+# **Fecha**: Junio de 2011
+
+# **Modelo**: Gunther Schosinsky
+
+# ## Simbología
+# 
+# fc: Capacidad de Infiltración.  
+# I: Infiltración.  
+# CC: Capacidad de Campo.  
+# PM: Punto de Marchitez.  
+# PR: Profundidad de Raices.  
+# (CC-PM): Rango de Agua Disponible.  
+# DS: Densidad de Suelo.  
+# C1: Factor de ETP, por cierre de estomas, antes que ocurra ETR.  
+# C2: Factor de ETP, por cierre de estomas, después que ocurre ETR.  
+# Kp: Factor por pendiente.  
+# Kv: Factor por vegetación.  
+# Kfc: Factor estimado con base a la prueba de infiltración. 
+# P: Precipitación Media Mensual.  
+# Pi: Precipitación que infilta.  
+# ESC: Escorrentía Superficial.  
+# ETP: Evapotranspiración Potencial.  
+# ETR: Evapotranspiración Real.  
+# HSi: Humedad de Suelo Inicial.  
+# HD: Humedad Disponible.  
+# HSf: Humedad de Suelo Final.  
+# DCC: Déficit de Capacidad de Campo.  
+# Rp: Recarga Potencial.  
+# NR: Necesidad de Riego.  
+# Ret: Retención de lluvia.
+
+# In[ ]:
+
+
+
+
