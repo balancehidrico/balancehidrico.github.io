d8ebc28a3015ba23efdd31a845ed1e0c73a3b46a
mfvargas <mfvargas@gmail.com>
2022-01-26 23:11:54 -0600

    Agregar referencias bibliográficas

--- _build/jupyter_execute/modelos/playapotrero-201106-schosinsky.py
+++ _build/jupyter_execute/modelos/playapotrero-201106-schosinsky.py
@@ -1,39 +1,42 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Balance hídrico de suelos
+# # Playa Potrero, Santa Cruz, Guanacaste (Zona Bosques 4). Junio 2011.
 
-# **Zona de estudio**: Playa Potrero, Santa Cruz - Guanacaste (Zona Bosques 4)  
-# **Fecha**: Junio de 2011
-
-# **Modelo**: Gunther Schosinsky
+# ## Modelo
+# Gunther Schosinsky {cite}`schosinsky_calculo_2006`
 
 # ## Simbología
 # 
-# fc: Capacidad de Infiltración.  
-# I: Infiltración.  
-# CC: Capacidad de Campo.  
-# PM: Punto de Marchitez.  
-# PR: Profundidad de Raices.  
-# (CC-PM): Rango de Agua Disponible.  
-# DS: Densidad de Suelo.  
-# C1: Factor de ETP, por cierre de estomas, antes que ocurra ETR.  
-# C2: Factor de ETP, por cierre de estomas, después que ocurre ETR.  
-# Kp: Factor por pendiente.  
-# Kv: Factor por vegetación.  
-# Kfc: Factor estimado con base a la prueba de infiltración. 
-# P: Precipitación Media Mensual.  
-# Pi: Precipitación que infilta.  
-# ESC: Escorrentía Superficial.  
-# ETP: Evapotranspiración Potencial.  
-# ETR: Evapotranspiración Real.  
-# HSi: Humedad de Suelo Inicial.  
-# HD: Humedad Disponible.  
-# HSf: Humedad de Suelo Final.  
-# DCC: Déficit de Capacidad de Campo.  
-# Rp: Recarga Potencial.  
-# NR: Necesidad de Riego.  
-# Ret: Retención de lluvia.
+# `fc`: Capacidad de Infiltración.  
+# `I`: Infiltración.  
+# `CC`: Capacidad de Campo.  
+# `PM`: Punto de Marchitez.  
+# `PR`: Profundidad de Raices.  
+# `(CC-PM)`: Rango de Agua Disponible.  
+# `DS`: Densidad de Suelo.  
+# `C1`: Factor de ETP, por cierre de estomas, antes que ocurra ETR.  
+# `C2`: Factor de ETP, por cierre de estomas, después que ocurre ETR.  
+# `Kp`: Factor por pendiente.  
+# `Kv`: Factor por vegetación.  
+# `Kfc`: Factor estimado con base a la prueba de infiltración. 
+# `P`: Precipitación Media Mensual.  
+# `Pi`: Precipitación que infilta.  
+# `ESC`: Escorrentía Superficial.  
+# `ETP`: Evapotranspiración Potencial.  
+# `ETR`: Evapotranspiración Real.  
+# `HSi`: Humedad de Suelo Inicial.  
+# `HD`: Humedad Disponible.  
+# `HSf`: Humedad de Suelo Final.  
+# `DCC`: Déficit de Capacidad de Campo.  
+# `Rp`: Recarga Potencial.  
+# `NR`: Necesidad de Riego.  
+# `Ret`: Retención de lluvia.
+
+# ## Referencias bibliográficas
+# ```{bibliography}
+# :filter: docname in docnames
+# ```
 
 # In[ ]:
 
